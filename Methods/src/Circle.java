e19f91bda9b0f06f613adc1d2761e6343c6f3bf1
Hiyeon <ohyeahbravo@gmail.com>
2017-10-18 16:53:16 +0200

    Methods Assignments

--- Methods/src/Circle.java
+++ Methods/src/Circle.java
@@ -1,5 +1,42 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
 
 public class Circle {
+	
 	//todo: add some methods to compute a circle's diameter, circumference and area.
 	//print some outputs to confirm you methods are working right.
+	
+	public static double diameter(double r) {
+		return 2*r;
+	}
+	
+	public static double circumference(double r) {
+		return Math.PI * 2 * r;
+	}
+	
+	public static double area(double r) {
+		return Math.PI * r * r;
+	}
+	
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		// Formatter for numbers
+		DecimalFormat formatter = new DecimalFormat("#0.00");
+		
+		// Get radius
+		System.out.println("Enter the radius: ");
+		double r = scan.nextDouble();
+		
+		// Use methods to calculate
+		double d = diameter(r);
+		double c = circumference(r);
+		double a = area(r);
+		
+		// Print results
+		System.out.println("Diameter: " + formatter.format(d));
+		System.out.println("Circumference: " + formatter.format(c));
+		System.out.println("Area: " + formatter.format(a));
+	}
 }
--- Methods/src/Prime.java
+++ Methods/src/Prime.java
@@ -3,11 +3,29 @@ import java.util.Scanner;
 
 public class Prime {
 	
+	// A method to tell whether it's a prime or not
+	public static boolean isPrime(int n) {
+		for(int i = 2; i < n; i++)
+			// if the number is dividable, it is not a prime
+			if (n % i == 0) 
+				return false;
+		return true;
+	}
+	
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		
 		//todo: write a method which tells you if a given number is a prime or not
 		
+		// Get an integer as an input
+		System.out.println("Give an integer: ");
+		int num = scan.nextInt();
+		
+		// Print the result
+		if(isPrime(num) == true)
+			System.out.println("It is a prime.");
+		else
+			System.out.println("It is NOT a prime.");
 	}
 	
 }
