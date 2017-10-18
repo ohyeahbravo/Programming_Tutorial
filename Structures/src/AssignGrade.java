cdab33a15acf83492896561dd0c46f139e3d8579
Hiyeon <ohyeahbravo@gmail.com>
2017-10-18 15:42:09 +0200

    First Two Assignments

--- Structures/src/AssignGrade.java
+++ Structures/src/AssignGrade.java
@@ -5,7 +5,51 @@ import java.util.Scanner;
 public class AssignGrade {
 	
 	public static void main(String[] args) {
+		
+		double score;
+		double grade = 0;
+		
+		System.out.print("Enter your score: ");
+		
 		Scanner scan = new Scanner(System.in);
+		
+		score = scan.nextDouble();
+		
+		if(score < 0 || score > 100) {
+			System.out.println("Score out of range.");
+			return;
+		}
+		
+		else if(score > 95)
+			grade = 1.0;
+		else if(score > 90)
+			grade = 1.3;
+		else if(score > 85)
+			grade = 1.7;
+		else if(score > 80)
+			grade = 2.0;
+		else if(score > 75)
+			grade = 2.3;
+		else if(score > 70)
+			grade = 2.7;
+		else if(score > 65)
+			grade = 3.0;
+		else if(score > 60)
+			grade = 3.3;
+		else if(score > 55)
+			grade = 3.7;
+		else if(score >= 50)
+			grade = 4.0;
+		else
+			grade = 5.0;
+		
+		// Print the grade
+		System.out.println("Your grade is " + grade);
+		
+		if(grade == 5.0)
+			System.out.println("You have failed. Sooorrryyy!");
+		else
+			System.out.println("You have passed. Well Done!");
 		
 		//todo: when executing the program you will be ask to write a score.
 		//The program converts the score then into a grade and prints that grade (1.0, 1.3, 1.7, ...)
--- Structures/src/EvenOdd.java
+++ Structures/src/EvenOdd.java
@@ -5,8 +5,17 @@ import java.util.Scanner;
 public class EvenOdd {
 
 	public static void main(String[] args) {
+		
 		Scanner scan = new Scanner(System.in);
 		
+		System.out.print("Enter the number: ");
+		int input = scan.nextInt();
+		
+		for(int count = 0; count <= input; count++) {
+			if(count % 2 == 1)
+				System.out.println(count);
+		}
+			
 		//todo: The program takes a number as input.
 		//It then goes through all integer values till reaching that value and prints all the odd ones.
 		
